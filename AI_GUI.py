73a469e7d982f602bdf77c64b36f96c7fb972c3e
Adit-dev874 <adityathecoder874@gmail.com>
2025-08-03 16:04:04 +0530

    Little changes

--- AI_GUI.py
+++ AI_GUI.py
@@ -1,6 +1,4 @@
 # .\AI_GUI\Scripts\Activate.ps1
 
-import subprocess           # To run ollama commands (like `ollama run llama2`)
-import customtkinter as ctk      # For building a modern-looking GUI
-import tkinter.messagebox as messagebox     # For showing alerts or errors
+
 
